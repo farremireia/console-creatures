ac40e80c4ac7874fbd5b354f7bf3ee1c8b574a92
Mireia Farre <67146405+farremireia@users.noreply.github.com>
2020-09-05 15:59:33 +0100

    Add files via upload

---
+++ ConsoleCreatures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Joe the Ghost");
+        }
+    }
+}
